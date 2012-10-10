1f7dab0655130a85850a8f4ad5df2b9204253a9c
Eugene Prikazchikov <eprikazc@gmail.com>
2012-10-10 15:28:54 +0400

    Initial commit

---
+++ index.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask, render_template, make_response, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("dashboard.html")
+
+@app.route('/data_update', methods=['GET'])
+def system_parameters():
+    data_type = request.args.get("data")
+    if data_type == "system_parameters":
+        filename_prefix = "erc_system_parameters_"
+    elif data_type == "adequacy":
+        filename_prefix = "erc_adequacy_table_"
+    else:
+        raise Exception("Invalid type: %s" %data_type)
+    data_dir = os.path.join(os.path.dirname(__file__), 'data')
+    last_file_name = sorted([f for f in os.listdir(data_dir) if f.startswith(filename_prefix)])[-1]
+    response = make_response(
+        open(os.path.join(data_dir, last_file_name)).read()
+    )
+    response.headers["Content-type"] = "text/plain"
+    return response
+
+
+@app.route('/adequacy', methods=['GET'])
+def adequacy():
+    data_dir = os.path.join(os.path.dirname(__file__), 'data')
+    last_file_name = sorted([f for f in os.listdir(data_dir) if f.startswith("erc_adequacy_table_")])[-1]
+    response = make_response(
+        open(os.path.join(data_dir, last_file_name)).read()
+    )
+    response.headers["Content-type"] = "text/plain"
+    return response
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
